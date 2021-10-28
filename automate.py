4ae5aa3a4c00c5d127f072e707d210281cc1b07f
JakeWMueller <jake.william.mueller@gmail.com>
2021-10-28 18:04:07 -0500

    Switched to Textbelt as it allows one free text a day, which is all I need

--- automate.py
+++ automate.py
@@ -1,23 +1,19 @@
+import requests
 import random
+from super_secret import phone
 import schedule
-import time
-from twilio.rest import Client
-from twilio_credentials import cellphone, twilio_account, twilio_number, twilio_token
-
 
 quotes_list = open("quotes.txt")
 quotes = quotes_list.readlines()
 quotes_list.close()
+quote = 'Good morning, Grandpa! Here is the quote of the day: ' + (quotes[random.randint(0, len(quotes)-1)])
 
+resp = requests.post('https://textbelt.com/text', {
+    'phone': phone,
+    'message': quote,
+    'key': 'textbelt',
+})
 
-def send_message(quotes):
-    account = twilio_account
-    token = twilio_token
-    client = Client(account,token)
-    quote = quotes[random.randint(0, len(quotes)-1)]
+schedule.every().day.at("06:00").do(resp)
 
-    client.messages.create(to=cellphone,
-                        from_=twilio_number,
-                        body= print("Good morning, Grandpa! Here is today's quote: " + quote))
-
-schedule.every().day.at("06:00").do(send_message, quotes)
+print(resp.json())
